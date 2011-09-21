4d729e80b0886e94c741356a9d7a0c35d9ef830a
Charles Feduke <charles.feduke@gmail.com>
2011-09-21 09:42:57 -0400

    initial commit

---
+++ RentalManager/RentalManagerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RentalManagerAppDelegate.h
+//  RentalManager
+//
+//  Created by Charles Feduke on 9/21/11.
+//  Copyright 2011 Deployment Zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RentalManagerAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ RentalManager/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  RentalManager
+//
+//  Created by Charles Feduke on 9/21/11.
+//  Copyright 2011 Deployment Zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
